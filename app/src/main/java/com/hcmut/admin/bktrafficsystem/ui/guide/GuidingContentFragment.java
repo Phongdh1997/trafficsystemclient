4b37a92e352e37af0717f3523c686b6791df4945
quoc duong <quocduong0897@gmail.com>
2020-11-15 23:13:00 +0700

    guide page ui

--- app/src/main/java/com/hcmut/admin/bktrafficsystem/ui/guide/GuidingContentFragment.java
+++ app/src/main/java/com/hcmut/admin/bktrafficsystem/ui/guide/GuidingContentFragment.java
@@ -1,17 +1,22 @@
 package com.hcmut.admin.bktrafficsystem.ui.guide;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Html;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.hcmut.admin.bktrafficsystem.R;
 import com.hcmut.admin.bktrafficsystem.ui.map.MapActivity;
@@ -28,14 +33,21 @@ public class GuidingContentFragment extends Fragment implements MapActivity.OnBa
     private static final String ARG_PARAM2 = "param2";
 
     public static final String GUIDING_FEATURE_TYPE = "guiding_feature_type";
-    public static final int SEARCH_DIRECTION = 1;
+    public static final int SEARCH_DIRECTION = 3;
     public static final int WARNING_STATUS = 2;
-    public static final int REPORT_STATUS = 3;
-    public static final int ACCOUNG_SETTING = 4;
+    public static final int REPORT_STATUS = 1;
+    public static final int ACCOUNG_SETTING = 0;
 
     private ImageView imgBack;
     private TextView txtTitle;
-    private TextView txtContent;
+    //private TextView txtContent;
+    private ViewPager viewPager;
+    private LinearLayout dotLayout;
+    private SliderAdapter sliderAdapter;
+    private TextView[] mdots;
+    private Button btnBackSlide;
+    private Button btnNextSlide;
+    private int currentSlideItem;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -90,21 +102,11 @@ public class GuidingContentFragment extends Fragment implements MapActivity.OnBa
         if (bundle != null) {
             int guidingType = bundle.getInt(GUIDING_FEATURE_TYPE, 0);
             Log.e("guiding type", "" + guidingType);
-            switch (guidingType){
-                case SEARCH_DIRECTION:
-                    createContent(R.string.searchWayTitle,R.string.searchWay);
-                    break;
-                case WARNING_STATUS:
-                    createContent(R.string.warningStatusTitle,R.string.warningStatusContent);
-                    break;
-                case REPORT_STATUS:
-                    createContent(R.string.reportTitle,R.string.reportContent);
-                    break;
-                case ACCOUNG_SETTING:
-                    createContent(R.string.accountSettingTitle,R.string.accountSettingContent);
-                    break;
-            }
+
+            createSliderGuide(view,  guidingType);
         }
+
+
     }
 
     private void addEvents() {
@@ -114,18 +116,93 @@ public class GuidingContentFragment extends Fragment implements MapActivity.OnBa
                 NavHostFragment.findNavController(GuidingContentFragment.this).popBackStack();
             }
         });
+        btnNextSlide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(++currentSlideItem);
+            }
+        });
+        btnBackSlide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(--currentSlideItem);
+            }
+        });
     }
 
-    private void createContent(final int title, final int conTent){
-        txtTitle.setText(title);
-        txtContent.setText(conTent);
+    private void createSliderGuide(View view, int poisition){
+        viewPager = (ViewPager) view.findViewById(R.id.slideViewPager);
+        sliderAdapter = new SliderAdapter(getContext());
+        viewPager.setAdapter(sliderAdapter);
+        viewPager.setCurrentItem(poisition);
+        addDotIndicator(poisition);
+        viewPager.addOnPageChangeListener(viewListener);
+        currentSlideItem = poisition;
+        changeBackNextStatus(currentSlideItem);
     }
 
     private void addControls(View view) {
         imgBack = view.findViewById(R.id.imgBack);
-        txtTitle = view.findViewById(R.id.txtSettingTitle);
-        txtContent = view.findViewById(R.id.txtContentGuide);
+        txtTitle = view.findViewById(R.id.txtGuideTitle);
+        dotLayout = (LinearLayout) view.findViewById(R.id.dotLayout);
+        btnBackSlide = view.findViewById(R.id.btnBackSlideGuide);
+        btnNextSlide = view.findViewById(R.id.btnNextSlideGuide);
+
     }
+
+    private void addDotIndicator(int poisition){
+        mdots = new TextView[4];
+        dotLayout.removeAllViews();
+        for(int i = 0; i < mdots.length; i++){
+            mdots[i] = new TextView(getContext());
+            mdots[i].setText(Html.fromHtml("&#8226;"));
+            mdots[i].setTextSize(35);
+            mdots[i].setTextColor(Color.parseColor("#A0A0A0"));
+            dotLayout.addView(mdots[i]);
+        }
+        if(mdots.length > 0){
+            mdots[poisition].setTextColor(Color.parseColor("#484848"));
+        }
+    }
+
+    ViewPager.OnPageChangeListener viewListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDotIndicator(position);
+            currentSlideItem = position;
+            changeBackNextStatus(currentSlideItem);
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
+
+    private void changeBackNextStatus(int currentSlideItem) {
+        if(currentSlideItem == 0){
+            btnBackSlide.setEnabled(false);
+            btnNextSlide.setEnabled(true);
+            btnNextSlide.setVisibility(View.VISIBLE);
+            btnBackSlide.setVisibility(View.INVISIBLE);
+        }else if(currentSlideItem == (mdots.length -1)){
+            btnNextSlide.setEnabled(false);
+            btnBackSlide.setEnabled(true);
+            btnNextSlide.setVisibility(View.INVISIBLE);
+            btnBackSlide.setVisibility(View.VISIBLE);
+        }else{
+            btnNextSlide.setEnabled(true);
+            btnBackSlide.setEnabled(true);
+            btnNextSlide.setVisibility(View.VISIBLE);
+            btnBackSlide.setVisibility(View.VISIBLE);
+        }
+    }
+
 
     @Override
     public void onBackPress() {
